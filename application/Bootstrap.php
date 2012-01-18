a68e659c4c76b99fd3ffe5209f10905d2b6dc6be
Stefan Warman <stefan.warman@gmail.com>
2012-01-18 23:18:19 +0000

    Various style changes to better support IE.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,11 +2,6 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initDoctype(){
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
-    }
     
     protected function _initNamespaces() {
         Zend_Loader_Autoloader::getInstance()->registerNamespace('ModelValidation_');
