5bb6341db0680729a64c4e9119c7a244e3321181
Stefan Warman <stefan.warman@gmail.com>
2012-01-19 22:48:56 +0000

    Refactored Util_Timestamp to allow rounding of results. Added additional data to views (See previous commit)

--- library/Util/Timestamp.php
+++ library/Util/Timestamp.php
@@ -7,16 +7,27 @@
 abstract class Util_Timestamp {
     
     public static function secondsToLabel($seconds){        
+        
         switch(TRUE):
             case ($seconds <= 60):
-                return $seconds.' seconds';
+                $value = $seconds;
+                $unit = 'seconds';
+                break;
             case ($seconds > 60 && $seconds <= 3600):
-                return ($seconds / 60).' minutes';
+                $value =($seconds / 60);
+                $unit = 'minutes';
+                break;
             case ($seconds > 3600 && $seconds <= 86400):
-                return ($seconds / 60 / 60).' hours';        
+                $value = ($seconds / 60 / 60);
+                $unit = 'hours';
+                break;                
             case ($seconds > 86400):
-                return ($seconds / 60 / 60 / 24).' days';       
+                $value = ($seconds / 60 / 60 / 24);
+                $unit = 'days';
+                break;       
         endswitch;
+        
+        return round($value, 2).' '.$unit;
     }
     
 }
